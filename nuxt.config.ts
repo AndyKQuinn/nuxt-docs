f27523d7e037a424f9af298330393abf4fce15ad
Andy Quinn <andy.kettler.quinn@gmail.com>
2024-07-27 22:22:39 -0500

    adjust build command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   extends: "@nuxt-themes/docus",
-  nitro: {
-    preset: "vercel-edge",
-  },
 })
